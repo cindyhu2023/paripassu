0357671ee4ca7caf15b00aa2493f738154babb61
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-04-24 11:29:21 -0500

    example filter to show only landmarks with names

--- js/pokemon-app.js
+++ js/pokemon-app.js
@@ -1,150 +1,143 @@
-const MAP_SIZE = 500
-const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056])
+const MAP_SIZE = 500;
+const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056]);
 
 // downtown center, uncomment to use downtown instead, or make your own
 // const NU_CENTER = ol.proj.fromLonLat([-87.6813, 42.049])
-const AUTOMOVE_SPEED = 1
-const UPDATE_RATE = 100
+const AUTOMOVE_SPEED = 1;
+const UPDATE_RATE = 100;
 /*
  Apps are made out of a header (title/controls) and footer
  and some number of columns
  If its vertical, the columns can become sections in one column
  */
 
-
-let landmarkCount = 0
+let landmarkCount = 0;
 
 let gameState = {
-	points: 0,
-	captured: [],
-	messages: []
-}
+  points: 0,
+  captured: [],
+  messages: [],
+};
 
 // Create an interactive map
 // Change any of these functions
 
 let map = new InteractiveMap({
-	mapCenter: NU_CENTER,
+  mapCenter: NU_CENTER,
 
-	// Ranges
-	ranges: [500, 200, 90, 1], // must be in reverse order
+  // Ranges
+  ranges: [500, 200, 90, 1], // must be in reverse order
 
-	initializeMap() {
-		// A good place to load landmarks
-		this.loadLandmarks("landmarks-shop-evanston", (landmark) => {
-			// Keep this landmark?
+  initializeMap() {
+    // A good place to load landmarks
+    this.loadLandmarks("landmarks-interesting-nu", (landmark) => {
+      // Keep this landmark?
 
-			// Keep all landmarks in the set
-			return true
+      // Keep all landmarks in the set
+      return landmark.openMapData.name;
 
-			// Only keep this landmark if its a store or amenity, e.g.
-			// return landmark.properties.amenity || landmark.properties.store
-		})
+      // Only keep this landmark if its a store or amenity, e.g.
+      // return landmark.properties.amenity || landmark.properties.store
+    });
 
-		// Create random landmarks
-		// You can also use this to create trails or clusters for the user to find
-		for (var i = 0; i < 10; i++) {
+    // Create random landmarks
+    // You can also use this to create trails or clusters for the user to find
+    // for (var i = 0; i < 10; i++) {
+    //   // make a polar offset (radius, theta)
+    //   // from the map's center (units are *approximately* meters)
+    //   let position = clonePolarOffset(
+    //     NU_CENTER,
+    //     400 * Math.random() + 300,
+    //     20 * Math.random()
+    //   );
+    //   this.createLandmark({
+    //     pos: position,
+    //     name: words.getRandomWord(),
+    //   });
+    // }
+  },
 
-			// make a polar offset (radius, theta) 
-			// from the map's center (units are *approximately* meters)
-			let position = clonePolarOffset(NU_CENTER, 400*Math.random() + 300, 20*Math.random())
-			this.createLandmark({
-				pos: position,
-				name: words.getRandomWord(),
-			})
-		}
-	},
+  update() {
+    // Do something each frame
+  },
 
-	update() {
-		// Do something each frame
-	},
+  initializeLandmark: (landmark, isPlayer) => {
+    // Add data to any landmark when it's created
 
-	initializeLandmark: (landmark, isPlayer) => {
-		// Add data to any landmark when it's created
+    // Any openmap data?
+    if (landmark.openMapData) {
+      console.log(landmark.openMapData);
+      landmark.name = landmark.openMapData.name;
+    }
 
-		// Any openmap data?
-		if (landmark.openMapData) {
-			console.log(landmark.openMapData)
-			landmark.name = landmark.openMapData.name
-		}
-		
-		// *You* decide how to create a marker
-		// These aren't used, but could be examples
-		landmark.idNumber = landmarkCount++
-		landmark.color = [Math.random(), 1, .5]
+    // *You* decide how to create a marker
+    // These aren't used, but could be examples
+    landmark.idNumber = landmarkCount++;
+    landmark.color = [Math.random(), 1, 0.5];
 
-		// Give it a random number of points
-		landmark.points = Math.floor(Math.random()*10 + 1)
-		return landmark
-	}, 
+    // Give it a random number of points
+    landmark.points = Math.floor(Math.random() * 10 + 1);
+    return landmark;
+  },
 
-	onEnterRange: (landmark, newLevel, oldLevel, dist) => {
-		// What happens when the user enters a range
-		// -1 is not in any range
+  onEnterRange: (landmark, newLevel, oldLevel, dist) => {
+    // What happens when the user enters a range
+    // -1 is not in any range
 
-		console.log("enter", landmark.name, newLevel)
-		if (newLevel == 2) {
+    console.log("enter", landmark.name, newLevel);
+    if (newLevel == 2) {
+      // Add points to my gamestate
+      gameState.points += landmark.points;
 
-			// Add points to my gamestate
-			gameState.points += landmark.points
+      // Have we captured this?
+      if (!gameState.captured.includes(landmark.name)) {
+        gameState.captured.push(landmark.name);
+        // Add a message
+        gameState.messages.push(
+          `You captured ${landmark.name} for ${landmark.points} points`
+        );
+      }
+    }
+  },
 
-			
+  onExitRange: (landmark, newLevel, oldLevel, dist) => {
+    // What happens when the user EXITS a range around a landmark
+    // e.g. (2->1, 0->-1)
 
-			// Have we captured this?
-			if (!gameState.captured.includes(landmark.name)) {
-				gameState.captured.push(landmark.name)
-				// Add a message
-				gameState.messages.push(`You captured ${landmark.name} for ${landmark.points} points`)
-			}
+    console.log("exit", landmark.name, newLevel);
+  },
 
-		}
-	},
+  featureToStyle: (landmark) => {
+    // How should we draw this landmark?
+    // Returns an object used to set up the drawing
 
-	onExitRange: (landmark, newLevel, oldLevel, dist) => {
-		// What happens when the user EXITS a range around a landmark 
-		// e.g. (2->1, 0->-1)
-		
-		console.log("exit", landmark.name, newLevel)
-	},
-	
-	
-	featureToStyle: (landmark) => {
-		// How should we draw this landmark?
-		// Returns an object used to set up the drawing
+    if (landmark.isPlayer) {
+      return {
+        icon: "person_pin_circle",
+        noBG: true, // skip the background
+      };
+    }
 
-		if (landmark.isPlayer) {
-			return {
-				icon: "person_pin_circle",
-				noBG: true // skip the background
-			}
-		}
-		
-		// Pick out a hue, we can reuse it for foreground and background
-		let hue = landmark.points*.1
-		return {
-			label: landmark.name + "\n" + landmark.distanceToPlayer +"m",
-			fontSize: 8,
+    // Pick out a hue, we can reuse it for foreground and background
+    let hue = landmark.points * 0.1;
+    return {
+      label: landmark.name + "\n" + landmark.distanceToPlayer + "m",
+      fontSize: 8,
 
-			// Icons (in icon folder)
-			icon: "person_pin_circle",
+      // Icons (in icon folder)
+      icon: "person_pin_circle",
 
-			// Colors are in HSL (hue, saturation, lightness)
-			iconColor: [hue, 1, .5],
-			bgColor: [hue, 1, .2],
-			noBG: false // skip the background
-		}
-	},
-
-	
-})
-
-
+      // Colors are in HSL (hue, saturation, lightness)
+      iconColor: [hue, 1, 0.5],
+      bgColor: [hue, 1, 0.2],
+      noBG: false, // skip the background
+    };
+  },
+});
 
 window.onload = (event) => {
-
-
-	const app = new Vue({
-		template: `
+  const app = new Vue({
+    template: `
 		<div id="app">
 		<header></header>
 			<div id="main-columns">
@@ -164,23 +157,20 @@ window.onload = (event) => {
 		<footer></footer>
 		</div>`,
 
-		data() {
-			return {
-			
-				map: map,
-				gameState: gameState
-			}
-		},
+    data() {
+      return {
+        map: map,
+        gameState: gameState,
+      };
+    },
 
-		// Get all of the intarsia components, plus various others
-		components: Object.assign({
-			// "user-widget": userWidget,
-			// "room-widget": roomWidget,
-			"location-widget": locationWidget,
-		}),
+    // Get all of the intarsia components, plus various others
+    components: Object.assign({
+      // "user-widget": userWidget,
+      // "room-widget": roomWidget,
+      "location-widget": locationWidget,
+    }),
 
-		el: "#app"
-	})
-
+    el: "#app",
+  });
 };
-
